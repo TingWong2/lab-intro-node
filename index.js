0804c16d3898fe72acab872ac98959be8a266528
Ting Wong <ting_wong2@yahoo.fr>
2022-01-09 17:05:22 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+    this.item = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.item.push(item);
+    this.item.sort((a,b) => a - b));
+  }
+   
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw outOfBounds();
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+    throw new EmptySortedList()
+  }
+  return this.item[this.length-1];
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if (!this.length) {
+      throw new EmptySortedList()
+    }
+    return this.item[0];
+  }
 
-  avg() {}
-}
+  sum() {
+    return this.item.reduce((acc, v) => acc + v , 0)
+  }
+
+  avg() {
+    if (!this.length) {
+      throw new EmptySortedList
+    }
+    return this.sum()/ this.length
+  }
+
 
 module.exports = SortedList;
